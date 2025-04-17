2a42ee6ed4270605dc211fa85a11215973703a8d
tomerbr3456 <59730830+tomerbr3456@users.noreply.github.com>
2025-04-17 10:02:21 +0300

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,18 @@
+
+from setuptools import setup, find_packages
+
+setup(
+    name="repo-76",
+    version="0.1.0",
+    packages=find_packages(),
+    install_requires=[
+        "requests>=2.25.1",
+        "pandas>=1.2.0",
+        "numpy>=1.20.0",
+    ],
+    author="",
+    author_email="",
+    description="Pytest based cloud 76 in Python",
+    keywords="repo-76, python",
+    url="",
+)
